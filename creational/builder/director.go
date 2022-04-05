bef1685d0cb50a3718201c9451465a694bd4f6ef
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-05 09:10:45 +0700

    Add dependency for buildProcess

---
+++ creational/builder/director.go
@@ -0,0 +1,13 @@
+package builder
+
+// Use as a dependency
+type ManufacturingDirector struct {
+}
+
+func (m *ManufacturingDirector) Construct() {
+
+}
+
+func (m *ManufacturingDirector) SetBuilder(b BuildProcess) {
+
+}
