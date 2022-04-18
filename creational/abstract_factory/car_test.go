0df04ea52cb87588a4d4a2841ce2701d055f1ab0
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-18 19:29:24 +0700

    Create 2 tests for CarFactory

--- creational/abstract_factory/car_test.go
+++ creational/abstract_factory/car_test.go
@@ -2,29 +2,55 @@ package abstract_factory
 
 import "testing"
 
-// TestCarFactory
-func TestCarFactory(t *testing.T) {
-	// Get the CarFactory.
+// TestLuxuryCarType executes a sets of sequence to create a LuxuryCar.
+func TestLuxuryCarType(t *testing.T) {
+	// Get the CarFactory, implementation of VehicleFactory.
 	carFactory, err := BuildFactory(CarFactoryType)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Get the LuxuryCar with Vehicle type.
+	// Get the LuxuryCar from CarFactory, implementation of Vehicle.
 	carVehicle, err := carFactory.NewVehicle(LuxuryCarType)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t.Logf(
-		"Car vehicle seats: %d, wheels: %d\n",
+		"LuxuryCar has seats: %d, wheels: %d\n",
 		carVehicle.NumSeats(),
 		carVehicle.NumWheels(),
 	)
 
-	// Assert LuxuryCar to Car type.
+	// Assert LuxuryCar to Car type, needs to invoke .NumDoors() method.
 	luxuryCar, ok := carVehicle.(Car)
 	if !ok {
-		t.Fatal("Struct assertion has failed")
+		t.Fatal("Struct assertion has failed.")
 	}
 	t.Logf("Luxury car has doors: %d\n", luxuryCar.NumDoors())
+}
+
+// TestFamilyCarType executes a sets of sequence to create a FamilyCar.
+func TestFamilyCarType(t *testing.T) {
+	// Get the CarFactory
+	carFactory, err := BuildFactory(CarFactoryType)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Get the FamilyCar from CarFactory
+	carVehicle, err := carFactory.NewVehicle(FamilyCarType)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf(
+		"FamilyCar has seats: %d, wheels: %d\n",
+		carVehicle.NumSeats(),
+		carVehicle.NumWheels(),
+	)
+
+	familyCar, ok := carVehicle.(Car)
+	if !ok {
+		t.Fatal("Struct assertion has failed.")
+	}
+	t.Logf("Luxury car has doors: %d\n", familyCar.NumDoors())
 }
