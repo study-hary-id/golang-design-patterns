fb03e8dacf14a3227c27bfc7060973c0bde7d0a5
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-18 13:45:56 +0700

    Refactor factory method pattern

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-// PaymentMethod is an abstraction layer of Factory Method.
+// PaymentMethod is an abstraction of each payment method.
 type PaymentMethod interface {
 	Pay(amount float32) string
 }
@@ -23,28 +23,39 @@ func GetPaymentMethod(m int) (PaymentMethod, error) {
 	case Cash:
 		return new(CashPM), nil
 	case DebitCard:
+		// scenario: Replace DebitCardPM to CreditCardPM.
 		return new(CreditCardPM), nil
 	default:
 		return nil, errors.New(
-			fmt.Sprintf("error: payment method %d not recognized\n", m),
+			fmt.Sprintf("error: payment method %d not recognized", m),
 		)
 	}
 }
 
+/*
+Defined available payment method, need to add a new payment method?
+Just add below
+*/
+
 type CashPM struct{}
 
+// Pay prints the successful message of payment process based on the amount.
 func (c *CashPM) Pay(amount float32) string {
-	return fmt.Sprintf("%0.2f paid using cash\n", amount)
+	return fmt.Sprintf("%0.2f paid using cash", amount)
 }
 
+// This doesn't used, deprecated based from scenario.
 type DebitCardPM struct{}
 
+// Pay prints the successful message of payment process based on the amount.
 func (d *DebitCardPM) Pay(amount float32) string {
-	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
+	return fmt.Sprintf("%0.2f paid using debit card", amount)
 }
 
+// CreditCardPM is a replacement payment method for DebitCardPM.
 type CreditCardPM struct{}
 
+// Pay prints the successful message of payment process based on the amount.
 func (d *CreditCardPM) Pay(amount float32) string {
-	return fmt.Sprintf("%0.2f paid using debit card (new)\n", amount)
+	return fmt.Sprintf("%0.2f paid using debit card (new)", amount)
 }
--- creational/factory/factory_test.go
+++ creational/factory/factory_test.go
@@ -5,18 +5,52 @@ import (
 	"testing"
 )
 
-// TestCreatePaymentMethodCash calls GetPaymentMethod with Cash option.
+func TestGetAvailablePaymentMethod(t *testing.T) {
+	payment, err := GetPaymentMethod(Cash)
+	if err != nil {
+		t.Errorf(
+			"GetPaymentMethod(Cash) = %v, %s, nil",
+			payment,
+			err.Error(),
+		)
+	}
+
+	payment, err = GetPaymentMethod(DebitCard)
+	if err != nil {
+		t.Errorf(
+			"GetPaymentMethod(DebitCard) = %v, %s, nil",
+			payment,
+			err.Error(),
+		)
+	}
+}
+
+func TestNotExistsPaymentMethod(t *testing.T) {
+	payment, err := GetPaymentMethod(3)
+	if err == nil {
+		t.Errorf(
+			"GetPaymentMethod(3) = %v, want match for <nil>, error",
+			payment,
+		)
+	}
+	t.Logf("message: %v", err)
+}
+
 func TestCreatePaymentMethodCash(t *testing.T) {
 	payment, err := GetPaymentMethod(Cash)
 	if err != nil {
-		t.Fatalf("GetPaymentMethod() = %v, want match for Cash, nil", err)
+		t.Fatalf(
+			"GetPaymentMethod(Cash) = %v, %v, nil",
+			payment,
+			err,
+		)
 	}
 
 	message := payment.Pay(10.30)
 	want := "paid using cash"
 	if !strings.Contains(message, want) {
 		t.Errorf(
-			"payment.Pay() = '%v',\nwant match for '%v', nil",
+			"payment.Pay() = %q,\nshould contains %q, nil",
 			message,
 			want,
 		)
@@ -25,31 +59,25 @@ func TestCreatePaymentMethodCash(t *testing.T) {
 	t.Log("log:", message)
 }
 
-// TestCreatePaymentMethodDebitCard calls GetPaymentMethod with DebitCard option.
 func TestCreatePaymentMethodDebitCard(t *testing.T) {
 	payment, err := GetPaymentMethod(DebitCard)
 	if err != nil {
-		t.Fatalf("GetPaymentMethod() = %v, want match for Debit Card, nil", err)
+		t.Fatalf(
+			"GetPaymentMethod(DebitCard) = %v, %v, nil",
+			payment,
+			err,
+		)
 	}
 
 	message := payment.Pay(22.30)
 	want := "paid using debit card"
 	if !strings.Contains(message, want) {
 		t.Errorf(
-			"payment.Pay() = '%v',\nwant match for '%v', nil",
+			"payment.Pay() = %q,\nwant match for %q, nil",
 			message,
 			want,
 		)
 	}
 
 	t.Log("log:", message)
-}
-
-// TestGetPaymentMethodNonExistent calls GetPaymentMethod with wrong option.
-func TestGetPaymentMethodNonExistent(t *testing.T) {
-	payment, err := GetPaymentMethod(3)
-	if err == nil {
-		t.Errorf("GetPaymentMethod(3) = %v, want match for error, nil", payment)
-	}
-	t.Log("log:", err)
 }
