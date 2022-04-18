4ffee47056c01cdcf32d95a7f1e4c9085f7a2ecb
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-18 20:28:56 +0700

    Add 2 possibilities tests

--- creational/prototype/prototype_test.go
+++ creational/prototype/prototype_test.go
@@ -1,13 +1,14 @@
 package prototype
 
-import "testing"
+import (
+	"testing"
+)
 
-// TestClone
-func TestClone(t *testing.T) {
+func TestCloneWhiteShirt(t *testing.T) {
 	// Get the ShirtCache instance.
 	shirtCache := GetShirtCloner()
 	if shirtCache == nil {
-		t.Fatal("GetShirtCloner() = nil")
+		t.Fatal("GetShirtCloner() = nil, shirtCache shouldn't be nil")
 	}
 
 	// Clone Shirt as an ItemInfoGetter type.
@@ -16,35 +17,88 @@ func TestClone(t *testing.T) {
 		t.Error(err)
 	}
 	if item == whitePrototype {
-		t.Error("item can't be equal to whitePrototype")
+		t.Error("error: item can't be equal to whitePrototype")
 	}
 
 	// Assert shirt from ItemInfoGetter to Shirt type.
 	shirt, ok := item.(*Shirt)
 	if !ok {
-		t.Fatal("Type assertion for shirt has failed")
+		t.Fatal("error: type assertion for shirt has failed")
 	}
-
 	shirt.SKU = "abc"
+
+	/* Second prototyping process */
 	// Create a second clone for white shirt.
 	secondItem, err := shirtCache.GetClone(White)
 	if err != nil {
 		t.Error(err)
 	}
+
 	secondShirt, ok := secondItem.(*Shirt)
 	if !ok {
-		t.Fatal("Type assertion for secondShirt has failed")
+		t.Fatal("error: type assertion for secondShirt has failed")
 	}
 	if shirt.SKU == secondShirt.SKU {
-		t.Error("SKU's shirt and secondShirt must be different")
+		t.Error("error: sku shirt and secondShirt must be different")
 	}
 	if shirt == secondShirt {
-		t.Error("shirt object cannot be equal to secondShirt object")
+		t.Error("error: shirt instance cannot be equal to secondShirt")
 	}
 
 	// Log the information of each shirt.
-	t.Logf("log: %s\n", shirt.GetInfo())
-	t.Logf("log: %s\n", secondShirt.GetInfo())
+	t.Logf("log: %s", shirt.GetInfo())
+	t.Logf("log: %s", secondShirt.GetInfo())
+	t.Logf(
+		"log: shirt memory positon: %p, secondShirt memory postion %p",
+		&shirt,
+		&secondShirt,
+	)
+}
+
+func TestCloneBlackShirt(t *testing.T) {
+	// Get the ShirtCache instance.
+	shirtCache := GetShirtCloner()
+	if shirtCache == nil {
+		t.Fatal("GetShirtCloner() = nil, shirtCache shouldn't be nil")
+	}
+
+	// Clone Shirt as an ItemInfoGetter type.
+	item, err := shirtCache.GetClone(Black)
+	if err != nil {
+		t.Error(err)
+	}
+	if item == whitePrototype {
+		t.Error("error: item can't be equal to whitePrototype")
+	}
+
+	// Assert shirt from ItemInfoGetter to Shirt type.
+	shirt, ok := item.(*Shirt)
+	if !ok {
+		t.Fatal("error: type assertion for shirt has failed")
+	}
+	shirt.SKU = "xyz"
+
+	/* Second prototyping process */
+	// Create a second clone for white shirt.
+	secondItem, err := shirtCache.GetClone(Black)
+	if err != nil {
+		t.Error(err)
+	}
+
+	secondShirt, ok := secondItem.(*Shirt)
+	if !ok {
+		t.Fatal("error: type assertion for secondShirt has failed")
+	}
+	if shirt.SKU == secondShirt.SKU {
+		t.Error("error: sku shirt and secondShirt must be different")
+	}
+	if shirt == secondShirt {
+		t.Error("error: shirt instance cannot be equal to secondShirt")
+	}
+
+	// Log the information of each shirt.
+	t.Logf("log: %s", shirt.GetInfo())
+	t.Logf("log: %s", secondShirt.GetInfo())
 	t.Logf(
 		"log: shirt memory positon: %p, secondShirt memory postion %p",
 		&shirt,
