8473d86a1022b99f712acd64c892a5ead437751d
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-18 12:55:26 +0700

    Add TestCarBuilderWithoutDirector

--- creational/builder/builder_test.go
+++ creational/builder/builder_test.go
@@ -1,17 +1,22 @@
 package builder
 
-import "testing"
+import (
+	"testing"
+)
 
-// TestCarBuilder tries to build a Car Product using CarBuilder.
+// TestCarBuilder build a car product using CarBuilder.
 func TestCarBuilder(t *testing.T) {
 	var (
 		manufacturingComplex = ManufacturingDirector{}
 		carBuilder           = &CarBuilder{}
 	)
+
+	// Need to pass a reference because manufacturingComplex needs to
+	// call CarBuilder's method within it.
 	manufacturingComplex.SetBuilder(carBuilder)
 	manufacturingComplex.Construct()
-
 	car := carBuilder.Build()
+
 	if car.Seats != 5 {
 		t.Errorf("car.Seats = %v, want match for 5, nil", car.Seats)
 	}
@@ -19,18 +24,21 @@ func TestCarBuilder(t *testing.T) {
 		t.Errorf("car.Wheels = %v, want match for 4, nil", car.Wheels)
 	}
 	if car.Structure != "Car" {
-		t.Errorf("car.Structure = '%v', want match for 'Car', nil", car.Structure)
+		t.Errorf("car.Structure = %q, want match for \"Car\", nil", car.Structure)
 	}
 }
 
-// TestBikeBuilder tries to build a Bike Product using BikeBuilder,
-// without using ManufacturingDirector.
+// TestBikeBuilder build a bike product using BikeBuilder,
 func TestBikeBuilder(t *testing.T) {
 	var (
-		bikeBuilder = &BikeBuilder{}
+		manufacturingComplex = ManufacturingDirector{}
+		bikeBuilder          = &BikeBuilder{}
 	)
 
-	bikeBuilder.SetSeats().SetStructure().SetWheels()
+	// Need to pass a reference because manufacturingComplex needs to
+	// call CarBuilder's method within it.
+	manufacturingComplex.SetBuilder(bikeBuilder)
+	manufacturingComplex.Construct()
 	bike := bikeBuilder.Build()
 
 	if bike.Seats != 2 {
@@ -40,6 +48,25 @@ func TestBikeBuilder(t *testing.T) {
 		t.Errorf("bike.Wheels = %v, want match for 2, nil", bike.Wheels)
 	}
 	if bike.Structure != "Bike" {
-		t.Errorf("bike.Structure = '%v', want match for 'Bike', nil", bike.Structure)
+		t.Errorf("bike.Structure = %q, want match for \"Bike\", nil", bike.Structure)
+	}
+}
+
+// TestCarBuilderWithoutDirector build a bike without ManufacturingDirector instance.
+func TestCarBuilderWithoutDirector(t *testing.T) {
+	carBuilder := &CarBuilder{}
+
+	// Constructing a car without director.
+	carBuilder.SetSeats().SetStructure().SetWheels()
+	car := carBuilder.Build()
+
+	if car.Seats != 5 {
+		t.Errorf("car.Seats = %v, want match for 5, nil", car.Seats)
+	}
+	if car.Wheels != 4 {
+		t.Errorf("car.Wheels = %v, want match for 4, nil", car.Wheels)
+	}
+	if car.Structure != "Car" {
+		t.Errorf("car.Structure = %q, want match for \"Car\", nil", car.Structure)
 	}
 }
